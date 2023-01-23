0d56e229b79a3a8fe09df5f466f3358f58b9028f
csantin <csantin@GCFRLY063>
2023-01-23 11:28:20 +0100

    Nina

---
+++ Nina.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tavernier
+{
+    internal class Nina
+    {
+        private int _Max_HP;
+        private int _Phys_Atk;
+        private int _Phys_Def;
+        private int _Accuracy;
+        private int _Critical;
+
+        public Nina() 
+        { }
+
+    }
+}
