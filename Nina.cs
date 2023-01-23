42930ec7f9577faaf9fc5bd843e3062c81c3f585
csantin <csantin@GCFRLY063>
2023-01-23 11:29:12 +0100

    ?

--- Nina.cs
+++ Nina.cs
@@ -13,10 +13,20 @@ namespace Tavernier
         private int _Phys_Atk;
         private int _Phys_Def;
         private int _Accuracy;
-        private int _Critical;
+        private int _Critical_Chance;
+        private int _Critical_Puiss;
+
+        private int _Max_SP;
+        private int _Elem_Atk;
+        private int _Elem_Def;
+        private int _Speed;
+        private int _Evasion;
 
         public Nina() 
-        { }
+        {
+
+        }
+
 
     }
 }
