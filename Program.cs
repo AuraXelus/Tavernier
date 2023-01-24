f4e594904604fb4da36f4c58aa078ee232b57095
unknown <jdoucet@gamingcampus.local>
2023-01-24 10:10:01 +0100

    add nain

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("               /\\_[]_/\\\r\n              |] _||_ [|\r\n       ___     \\/ || \\/\r\n      /___\\       ||\r\n     (|0 0|)      ||\r\n   __/{\\U/}\\_ ___/vvv\r\n  / \\  {~}   / _|_P|\r\n  | /\\  ~   /_/   []\r\n  |_| (____)        \r\n  \\_]/______\\               \r\n     _\\_||_/_           \r\n    (_,_||_,_)");
         }
     }
 }
