c0f3389e32cd994efa0fd6d6cbd32c279b7233d6
unknown <jdoucet@gamingcampus.local>
2023-01-25 11:59:33 +0100

    essai deplacement (non fonctionnel)

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
-﻿namespace Tavernier
+﻿using System;
+using System.Data;
+
+namespace Tavernier
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int x = 0;
             // personnage nain
             Console.WriteLine("               /\\_[]_/\\\r\n              |] _||_ [|\r\n       ___     \\/ || \\/\r\n      /___\\       ||\r\n     (|0 0|)      ||\r\n   __/{\\U/}\\_ ___/vvv\r\n  / \\  {~}   / _|_P|\r\n  | /\\  ~   /_/   []\r\n  |_| (____)        \r\n  \\_]/______\\               \r\n     _\\_||_/_           \r\n    (_,_||_,_)");
 
@@ -17,39 +21,51 @@
             Console.WriteLine("           .-----.\r\n \\ ' /   _/    )/\r\n- ( ) -('---''--)\r\n / . \\((()\\^_^/)()\r\n  \\\\_\\ (()_)-((()()\r\n   '- \\ )/\\._./(()\r\n     '/\\/( X   ) \\\r\n     (___)|___/ ) \\\r\n          |.#_|(___)\r\n         /\\    \\ ( (_\r\n         \\/\\/\\/\\) \\\\\r\n         | / \\ |\r\n         |(   \\|\r\n        _|_)__|_\\_\r\n        )...()...(\r\n         | (   \\ |     \r\n      .-'__,)  (  \\\r\n                '\\_-,");
 
             // Map
+            //Console.Write("###############################################################\n");
+            
             Console.Write("###############################################################\n");
-
-
+            Console.Write("#             P                                               #\n");
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 5; j++)
                 {
                     Console.Write("#                                                             #\n");
                 }
+
             }
             Console.Write("###############################################################\n");
-            if (Console.ReadKey().Key == ConsoleKey.UpArrow)
+            /*if (Console.ReadKey().Key == ConsoleKey.UpArrow)
             {
-                Console.Write("up");
+                Console.Write(perso);
+                x -= 1;
+                Console.Clear();
+                Console.Write(perso);
+
             }
 
             else if (Console.ReadKey().Key == ConsoleKey.DownArrow)
             {
-                Console.Write("down");
-                //perso(0, -1);
+                Console.Write(perso);
+                x += 1;
+                Console.Clear();
+                Console.Write(perso);
             }
 
             else if (Console.ReadKey().Key == ConsoleKey.LeftArrow)
             {
-                Console.Write("left");
-                //perso(0, -1);
+                Console.Write(perso);
+                x -= 1;
+                Console.Clear();
+                Console.Write(perso);
             }
 
             else if (Console.ReadKey().Key == ConsoleKey.RightArrow)
             {
-                Console.Write("right");
-                //perso(0, -1);
-            }
+                Console.Write(perso);
+                x += 1;
+                Console.Clear();
+                Console.Write(perso);
+            }*/
         }
     }
 }
