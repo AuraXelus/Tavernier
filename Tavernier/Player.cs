32e04327dfede3ce2d662a9cf22915d68b748dc5
Alexis Philippe <alexis.plp41@gmail.com>
2023-01-31 18:53:46 +0100

    map

---
+++ Tavernier/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tavernier
+{
+    public class Player
+    {
+        private char playerChar = 'P';
+        private int posX = 1;
+        private int posY = 1;
+
+        public Player() { }
+
+        public char PlayerChar { get => playerChar; set => playerChar = value; }
+        public int PosX { get => posX; set => posX = value; }
+        public int PosY { get => posY; set => posY = value; }
+    }
+}
--- Tavernier/Program.cs
+++ Tavernier/Program.cs
@@ -1,24 +1,77 @@
 ﻿using System.Net;
+using System.Numerics;
 using System.Security.Cryptography;
 
 namespace Tavernier
 {
     public class Program
     {
+        static char[,] initMap(char[,] map)
+        {
+            for (int y = 0; y < 25; y++)
+            {
+                for (int x = 0; x < 50; x++)
+                {
+                    if (y == 0 || y == 24) { map[y, x] = '#'; }
+                    else if (x == 0 || x == 49) { map[y, x] = '#'; }
+                    else { map[y, x] = ' '; }
+                }
+            }
+            return map;
+        }
+
+        static void run(Player justine, char[,] map)
+        {
+            ConsoleKey key = Console.ReadKey(true).Key;
+
+            if (key == ConsoleKey.UpArrow)
+            {
+                if (map[justine.PosX, justine.PosY - 1] != '#') justine.PosY--;
+            }
+            else if (key == ConsoleKey.DownArrow)
+            {
+                if (map[justine.PosX, justine.PosY + 1] != '#') justine.PosY++;
+            }
+            else if (key == ConsoleKey.LeftArrow)
+            {
+                if (map[justine.PosX - 1, justine.PosY] != '#') justine.PosX--;
+            }
+            else if (key == ConsoleKey.RightArrow)
+            {
+                if (map[justine.PosX + 1, justine.PosY] != '#') justine.PosX++;
+            }
+        }
+
+        static void diplayMap(Player justine, char[,] map)
+        {
+            for (int y = 0; y < 25; y++)
+            {
+                for (int x = 0; x < 50; x++)
+                {
+                    map = initMap(map);
+                    if (justine.PosX == x && justine.PosY == y) { map[y, x] = 'P'; }
+
+                    if (y == 0 || y == 24) { Console.Write(map[y, x]); }
+                    else if (x == 0 || x == 49) { Console.Write(map[y, x]); }
+                    else { Console.Write(map[y, x]); }
+
+
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
-            Nina Nina = new Nina();
-            Thran Thran = new Thran();
-            WarriorGoblin Goblin = new WarriorGoblin();
-            Fight_State FirstFight= new Fight_State();
-            FirstFight.run(Thran, Goblin);
+            bool fin = false;
+            char[,] map = new char[25, 50];
+            Player justine = new Player();
+            map = initMap(map);
+            diplayMap(justine, map);
+            do
+            {
+                run(justine, map);
+                diplayMap(justine, map);
+            } while (fin == false);
         }
     }
 }
-
-//Utilisation des faiblesses
-//Ajout de la magie
-//Objet (armes) potions vie, mana,
-//Niveau
-//System d'argent
-//Spécificité des compétence
